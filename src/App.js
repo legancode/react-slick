8d1852b0b0e5e3b6d7ada3d3c6a4e38ca85db8ef
Angel Salvador <angelsalvadordev@gmail.com>
2019-11-03 22:17:48 -0500

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Slider from "react-slick";
 
-function App() {
+const App = props => {
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  //   arrows: false,
+  //   touchThreshold: 7,
+  //   pauseOnHover: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 1,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     }
+  //   ]
+  // };
+
+  const settings2 = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    touchThreshold: 7, //sensibilidad del swipe
+    pauseOnHover: true,
+    centerMode: true,
+    centerPadding: '40px', //paddinga  los lados del slider
+    focusOnSelect: true,
+    dotsClass: 'dots-class'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ES UN HEADER
       </header>
+      <main>
+        {/* <div className="carousel">
+          <Slider {...settings} >
+            <div className="image">
+              <img src="https://images.unsplash.com/photo-1572550182267-9e3451010b01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="foto" />
+            </div>
+            <div className="image">
+              <img src="https://images.unsplash.com/photo-1572666352738-66cf42581fd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="foto" />
+            </div>
+            <div className="image">
+              <img src="https://images.unsplash.com/photo-1572656934803-d2162b2e98bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="foto" />
+            </div>
+            <div className="image">
+              <img src="https://images.unsplash.com/photo-1572666352738-66cf42581fd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="foto" />
+            </div>
+          </Slider>
+        </div> */}
+
+        <div className="carousel-2">
+          <Slider {...settings2} >
+            <div className="block">
+              <p>1</p>
+            </div>
+            <div className="block">
+              <p>2</p>
+            </div>
+            <div className="block">
+              <p>3</p>
+            </div>
+            <div className="block">
+              <p>4</p>
+            </div>
+            <div className="block">
+              <p>5</p>
+            </div>
+            <div className="block">
+              <p>6</p>
+            </div>
+            <div className="block">
+              <p>7</p>
+            </div>
+            <div className="block">
+              <p>8</p>
+            </div>
+            <div className="block">
+              <p>9</p>
+            </div>
+            <div className="block">
+              <p>10</p>
+            </div>
+          </Slider>
+        </div>
+      </main>
     </div>
   );
 }
